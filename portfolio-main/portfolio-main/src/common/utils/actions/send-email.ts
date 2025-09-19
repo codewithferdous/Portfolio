d03f04e447ffa308f4076ef5ea06c7031e5595c6
FERDOUS GULZAR <136057245+codewithferdous@users.noreply.github.com>
2025-09-19 16:21:31 +0500

    email

--- portfolio-main/portfolio-main/src/common/utils/actions/send-email.ts
+++ portfolio-main/portfolio-main/src/common/utils/actions/send-email.ts
@@ -5,7 +5,7 @@ import { Resend } from "resend";
 import ContactFormEmail from "@/common/components/sections/contact/_components/contact-form-email";
 import { getErrorMessage, validateString } from "@/common/lib/utils";
 
-const resend = new Resend("re_V6oPZfPJ_Nxn5aVGocUaaZDLJynppWroA");
+const resend = new Resend("re_TuSNF6sb_Ga3BM6cSPz51T7wYEQvQGi9g");
 
 export const sendEmail = async (formData: FormData) => {
   const senderEmail = formData.get("senderEmail");
@@ -24,16 +24,17 @@ export const sendEmail = async (formData: FormData) => {
 
   let data;
   try {
-    data = await resend.emails.send({
-      from: "onboarding@resend.dev",
-      to: ["asimalyas4440@gmail.com"],
-      subject: "Message from contact form | PORTFOLIO",
-      reply_to: senderEmail,
-      react: React.createElement(ContactFormEmail, {
-        message: message,
-        senderEmail: senderEmail,
-      }),
-    });
+  data = await resend.emails.send({
+  from: "onboarding@resend.dev", // must be a verified sender in Resend
+  to: ["ferdousgulzar543@gmail.com"], // <-- your email here
+  subject: "Message from contact form | PORTFOLIO",
+  reply_to: senderEmail,
+  react: React.createElement(ContactFormEmail, {
+    message: message,
+    senderEmail: senderEmail,
+  }),
+});
+
   } catch (error: unknown) {
     return {
       error: getErrorMessage(error),
